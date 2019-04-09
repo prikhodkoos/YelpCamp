c06fb4eb84b7274ecc641b7e7a7b6adf44590943
Svitlana Prykhodko <prikhodks@gmail.com>
2019-04-09 23:46:01 +0300

    Added some styles to Campgrounds Page

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const campgrounds = [
     {
         name: "Big Flower Camp",
-        img: "https://images.unsplash.com/photo-1534880606858-29b0e8a24e8d?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+        img: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     },
     {
         name: "All we need is Camp",
@@ -17,6 +17,34 @@ const campgrounds = [
     {
         name: "My little Camp",
         img: "https://images.unsplash.com/photo-1524494860062-9442631ee30e?ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
+    },
+    {
+        name: "Great Hopes",
+        img: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Mountaines and Dreams",
+        img: "https://images.unsplash.com/photo-1485809052957-5113b0ff51af?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Modern Camping",
+        img: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Bostom Lake",
+        img: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
+    },
+    {
+        name: "American Dream Camp",
+        img: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Mountaine Lovers",
+        img: "https://images.unsplash.com/photo-1530488345268-51e6128cb132?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Lakes and Fields",
+        img: "https://images.unsplash.com/photo-1508768516474-73606cb84ce2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     },
 ];
 

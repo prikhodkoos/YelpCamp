2c3113a812f50ee3a895d26c726a10f3e5867831
Svitlana Prykhodko <prikhodks@gmail.com>
2019-04-24 20:31:06 +0300

    Added ability to read more information about campgrounds, added description field

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const campgroundSchema = new mongoose.Schema({
     name: String,
     img: String,
+    description: String,
 });
 const Campground = mongoose.model("Campground", campgroundSchema);
 
@@ -21,7 +22,7 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", (req, res) => {
     Campground.find({}, (error, campgrounds) => {
         if (!error){
-            res.render("campgrounds", {campgrounds});
+            res.render("index", {campgrounds});
         }
         else {
             console.log(error);
@@ -38,7 +39,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post("/campgrounds", (req, res) => {
     Campground.create({
             name: req.body.name,
-            img: req.body.img
+            img: req.body.img,
+            description: req.body.description,
         }, 
         (error, item) => {
             if (!error) {
@@ -48,6 +50,18 @@ app.post("/campgrounds", (req, res) => {
             else {
                 console.log(error);
             }
+    });
+});
+
+// show route
+app.get("/campgrounds/:id", (req, res) => {
+    Campground.findById(req.params.id, (error, detailedCamp) => {
+        if (!error) {
+            res.render("show", {detailedCamp});
+        }
+        else {
+            console.log(error);
+        }
     });
 });
 
